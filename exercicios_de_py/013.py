84e51cbbc754be173ce88e158fc5b57e1310d697
ferreis <rafa_ferreis@hotmail.com>
2022-04-15 14:42:17 -0300

    Observação de melhoria

--- exercicios_de_py/013.py
+++ exercicios_de_py/013.py
@@ -1,5 +1,7 @@
 #Exe013 Faça um algoritmo que leia o salario de um funcionário e mostre seu novo salário, com 15% de aumento.
 salario=float(input('Seu Salario: '))
-aumento=salario*0.15
+aumento=salario*0.15 
 novo_salario=salario+aumento
-print('Seu salário de R${} ira ter um aumento de 15% que e R${}, seu novo salario sera de R${}'.format(salario,aumento,novo_salario))
+#ou novo_salario=salario*1.15 vai dar o mesmo resultado no final talvez seja mais pratico?...
+print('Seu salário de R${:.2f} ira ter um aumento de 15% que e R${:.2f}, seu novo salario sera de R${:.2f}'.format(salario,aumento,novo_salario))
+
