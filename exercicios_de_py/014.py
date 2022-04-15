15d484a76642e21c31f7726020392fc58d1ce9d8
ferreis <rafa_ferreis@hotmail.com>
2022-04-15 15:01:44 -0300

    Resolvido

--- exercicios_de_py/014.py
+++ exercicios_de_py/014.py
@@ -1,1 +1,4 @@
-#Exe014
+#Exe014 Escreva um programa que converta uma temperatura digitada em °c e converta para °f
+c =float(input('Digite a Temperatura celsius: '))
+f=c*9/5+32
+print('a temperatura {:.1f}°c fica {:.1f}°f'.format(c, f))
