186414f9e8275ba091fd39d654a0c5b0276db584
ferreis <rafa_ferreis@hotmail.com>
2022-04-15 14:31:44 -0300

    revisão

--- exercicios_de_py/012.py
+++ exercicios_de_py/012.py
@@ -1,3 +1,7 @@
 #Exe012 faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto.
 preco=float(input('Preço do Produto: '))
-print("Novo Preço com 5% de desconto ja incluido: R${}".format(preco*0.95))
+print("Novo Preço com 5% de desconto ja incluido: R${:.2f}".format(preco*0.95))
+#ou Guardando a info do desconto
+print('-------------------------------')
+desc=preco*.95
+print('Novo preço comm 5% de desconto ja incluido R${:.2f}'.format(desc))
