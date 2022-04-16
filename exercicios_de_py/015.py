6a95c76512b6a461eb87126bb2890341438e4142
ferreis <rafa_ferreis@hotmail.com>
2022-04-16 15:59:01 -0300

    Correçao

--- exercicios_de_py/015.py
+++ exercicios_de_py/015.py
@@ -6,4 +6,4 @@ km_rodado = float(input('Quantos Km rodados? '))
 valor_dia = dia * 60
 valor_km = km_rodado * 0.15
 valor_total = valor_dia + valor_km
-print('O carro voi Alugado por {} Dias no valor de R${:.2f}, e rodou por {} Km somando no valor de R${:.2f}\n Total do aluguel R${:.2f}'.format(dia,valor_dia,km_rodado,valor_km,valor_total))
+print('O carro foi Alugado por {} Dias no valor de R${:.2f}, e rodou por {}Km somando no valor de R${:.2f}\n Total do aluguel R${:.2f}'.format(dia,valor_dia,km_rodado,valor_km,valor_total))
