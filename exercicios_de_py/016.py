849498b5c7c2f101f205404944b558c1b6f9d0db
ferreis <rafa_ferreis@hotmail.com>
2022-04-16 21:37:18 -0300

    começando módulos

--- exercicios_de_py/016.py
+++ exercicios_de_py/016.py
@@ -1,1 +1,8 @@
-#Exe016
+#Exe016 Crie um programa que leia um número real qualquer pelo teclado e mostre na tela a sua porção interira
+from math import trunc
+
+num = float(input('Digite umm número: '))
+inum = trunc(num)
+print('Seu numero real é {}, e no inteiro é {} '.format(num, inum))
+# ou 
+print('Seu numero real é {}, e no inteiro é {} '.format(num, int(num)))
