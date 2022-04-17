3eaea51f2b40d8a7c419101dd8c31bb73a88b7eb
ferreis <rafa_ferreis@hotmail.com>
2022-04-17 20:04:42 -0300

    019 com import choicer

--- exercicios_de_py/019.py
+++ exercicios_de_py/019.py
@@ -1,10 +1,10 @@
 #Exe019 Um professor quer sortear um dos seus quatro alunos para apagar o quadro.
 #Faça um programa que ajude ele, lendo o nome dos alunos e escrevendo na tela o nome do escolhido.
-import random
+from random import choice
 nome1 = input('1° Aluno: ')
 nome2 = input('2° Aluno: ')
 nome3 = input('3° Aluno: ')
 nome4 = input('4° Aluno: ')
 lista = [nome1,nome2,nome3,nome4]
-escolhido = random.choice(lista)
+escolhido = choice(lista)
 print('O aluno escolhido foi {}'.format(escolhido))
